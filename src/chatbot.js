6ac76cf5d132b9a108f00599f74327c2863ff024
Aneeshbs44 <anish107655@gmail.com>
2024-11-24 22:56:02 +0530

    Added stylesheet for chatbot.js main file

--- src/chatbot.js
+++ src/chatbot.js
@@ -2,6 +2,9 @@ import axios from "axios";
 import ChatBubble from "./ChatBubble";
 import { speak, isSpeakingAsync, stop } from "expo-speech";
 import { useState } from "react";
+import { StyleSheet, TouchableOpacity } from "react-native";
+import { ActivityIndicator, FlatList } from "react-native-web";
+import { TextInput } from "react-native";
 
 const ChatBot = ()=>{
     const [chat,Setchat] = useState([]);
@@ -53,5 +56,99 @@ const handleUserInput = async()=>{
 }
 };
 const handleSpeech = async(text) =>{
+    if(isspeaking){
+        stop();
+        setisspeaking(false);
+    }else{
+        if(!(await isSpeakingAsync())){
+            speak(text);
+            setisspeaking(true);
+        }
+    }
+};
+    const renderChatItem = ({item})  =>{
+        <ChatBubble
+        role = {item.role}
+        text = {item.parts[0].text}
+        onspeech={()=> handleSpeech(item.parts[0].text)}
+        />
+    };
+    return(
+        <View style = {styles.container}>
+        <Text style = {styles.title}>Gemini Chatbot</Text>
+        <FlatList
+            data = {chat}
+            renderItem={renderChatItem}
+            keyExtractor={(item,index) => index.toString}
+            contentContainerStyle={styles.chatContainer}
+        />
+        <View style = {styles.inputContainer}>
+        <TextInput
+            style = {styles.input}
+            placeholder = "Type your message...."
+            placeholderTextColor="#aaa"
+            value={userinput}
+            onChangeText={setuserinput}
+            /> 
+        <TouchableOpacity style = {styles.button} onPress={handleUserInput}>
+            <Text style = {styles.buttonText}>Send</Text>
+        </TouchableOpacity>
+        </View>
+        {loading && <ActivityIndicator style = {styles.loading} color="#333"/> }
+        {error && <Text style = {styles.error}>{error}</Text>}
+        </View>
+    );
+};
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        padding:16,
+        background:"#f8f8f8",
+    },
+    title:{
+        fontsize:24,
+        fontWeight:"bold",
+        color:"#333",
+        marginBottom:20,
+        marginTop:40,
+        textAlign:'center',
+    },
+    chatContainer:{
+        flexGrow:1,
+        justifyContent:"flex-end",
+    },
+    inputContainer:{
+        flexDirection:"row",
+        alignItems:"center",
+        marginTop:10,
+    },
+    input:{
+        flex:1,
+        height:50,
+        marginRight:10,
+        padding:8,
+        borderColor:"#333",
+        borderWidth:1,
+        borderRadius:25,
+        color:"#333",
+        backgroundColor:"#fff"
+    },
+    buttonText:{
+        color:"#fff",
+        textAlign:"center",
+    },
+    loading:{
+        marginTop:10,
+    },
+    button:{
+        padding:10,
+        backgroundColor:"#007AFF",
+        borderRadius:25,
+    },
+    error:{
+        color:"red",
+        marginTop:10,
+    },
 
-}
+});
+export default ChatBot; 
